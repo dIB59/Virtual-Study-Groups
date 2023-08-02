ec20c6e65b1144057ddde34182daea9970817a58
Ibrahim <Ibrahim@ebs.net.pk>
2023-08-02 12:37:39 +0200

    init commit

---
+++ src/test/java/com/studygroup/demo/StudygroupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studygroup.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudygroupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
